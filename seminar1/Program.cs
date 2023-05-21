97fe6341f6c265c9e1218b929da489ae45492a42
Valeria Ivanova <valeriya.tamara.8@gmail.com>
2023-05-21 03:15:55 +0300

    Added code to seminar1

--- seminar1/Program.cs
+++ seminar1/Program.cs
@@ -1,6 +1,17 @@
-﻿Console.WriteLine("Enter your name");
-string username = Console.ReadLine();
-Console.WriteLine($"Hello {username}");
+﻿Console.WriteLine("Enter first number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter first number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine($"{num1} is greater than {num2}");
+}
+else
+{
+    Console.WriteLine(($"{num1} is less than {num2}"));
+};
+
+
 
 
 
